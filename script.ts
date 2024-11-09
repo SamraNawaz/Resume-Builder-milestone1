c19b85fb40e09df1a2f972bc9a0faaa4177e6ea8
Samra Nawaz <167023891+SamraNawaz@users.noreply.github.com>
2024-11-09 11:15:03 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,24 @@
+// .............Reference Portion.........
+
+let ref_section = document.getElementById('ref')
+let ref_btn = document.getElementById('ref_btn')
+
+ref_btn?.addEventListener('click', ()=>{
+    ref_section!.classList.toggle('hide')
+})
+
+// ........Experiance Portion.........
+let exp_section = document.getElementById('experience')
+let exp_btn = document.getElementById('exp_btn')
+
+
+exp_btn?.addEventListener('click', ()=>{
+    exp_section!.classList.toggle('hide')
+})
+
+//---------------------print button 
+let print_btn=document.getElementById('print_btn')
+
+print_btn?.addEventListener('click', ()=>{
+    window.print()
+})
